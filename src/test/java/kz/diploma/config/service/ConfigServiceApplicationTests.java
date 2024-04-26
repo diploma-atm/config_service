cbe7c48f53208a4ca4a691c6f5c0f7af1f1042bd
justdika <fragerrr73@gmail.com>
2024-04-26 23:59:28 +0500

    first commit

---
+++ src/test/java/kz/diploma/config/service/ConfigServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.diploma.config.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
